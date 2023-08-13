b4f4049b3b60ace95a4a898e70902d1aa1ab3fb4
Meet Rajput <rajputmeet10@gmail.com>
2023-08-13 14:16:25 +0530

    setup.py updated

--- Setup.py
+++ Setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Socket-Wizard",                     # This is the name of the package
+    name="SocketWizard",                     # This is the name of the package
     version="0.0.1",                        # The initial release version
     author="Meet Rajput",                     # Full name of the author
     description="Implementation of socket programming and many pentesting tools",
@@ -18,7 +18,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],                                      # Information to filter the project on PyPi website
     python_requires='>=3.6',                # Minimum version requirement of the package
-    py_modules=["Socket-Wizard"],             # Name of the python package
-    package_dir={'':'Socket-Wizard/src'},     # Directory of the source code of the package
+    py_modules=["SocketWizard"],             # Name of the python package
+    package_dir={'':'scripts/src'},     # Directory of the source code of the package
     install_requires=[]                     # Install other dependencies if any
 )
